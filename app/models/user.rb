e2d029f7501d2fa4a2958c58165f8e1482034e98
Vicentecorrea <vicorrea@miuandes.cl>
2019-03-27 20:44:47 -0300

    first method complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :orders
   def f()
-    #user_orders = Order.where(user_id: self.id)
-    #order_tickets = user_orders.where(order)
+    user_orders = Order.where(user_id: self.id).select(:id)
+    order_tickets = Ticket.where(order_id: user_orders).select(:price)
+    most_expensive_ticket = (order_tickets.order(:price).reverse_order).first
+    most_expensive_ticket
 
     #Instance method most_expensive_ticket_bought, which returns the price of the
     #most expensive ticket bought by the user.
