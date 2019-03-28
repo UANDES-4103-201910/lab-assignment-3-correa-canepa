7b66d8853a275d1deaeea92aa4cda471bc461a14
Matias Canepa <macanepa@miuandes.cl>
2019-03-28 14:05:16 -0300

    Method 3b modified

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,14 +6,14 @@ class Event < ApplicationRecord
 		most_sold = (Ticket.group(:event_id).count())
 		most_sold = most_sold.max_by{|k,v| v}
 		puts "event_id: #{most_sold[0]} cant: #{[most_sold[1]]}"
-		return most_sold[0]
+		return Event.find(most_sold[0])
 	end
 
   def highest_revenue
-		most_sold = (Ticket.group(:event_id).sum(:price))
-		most_sold = most_sold.max_by{|k,v| v}
-		puts "event_id: #{most_sold[0]} cant: #{[most_sold[1]]}"
-		return most_sold[0]
+		grouped_list = (Ticket.group(:event_id).sum(:price))
+		highest_revenue = grouped_list.max_by{|k,v| v}
+		puts "event_id: #{highest_revenue[0]} cant: #{[highest_revenue[1]]}"
+		return Event.find(highest_revenue[0])
 	end
 
 end
